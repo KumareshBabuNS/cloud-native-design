614b1e989870b8784ca77fae3ed0acb8aa965fcc
Cornelia Davis <cornelia@corneliadavis.com>
2017-02-12 12:10:20 -0800

    initial project structure

---
+++ cloudnativedesign-helloworld/src/main/java/com/corneliadavis/cloudnativedesign/CloudnativedesignHelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.corneliadavis.cloudnativedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudnativedesignHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudnativedesignHelloworldApplication.class, args);
+	}
+}
---
+++ cloudnativedesign-helloworld/src/test/java/com/corneliadavis/cloudnativedesign/CloudnativedesignHelloworldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.corneliadavis.cloudnativedesign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudnativedesignHelloworldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
